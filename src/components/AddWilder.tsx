ab0b71e31254bf2792561279e6aebac67fd37299
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2021-04-21 09:18:55 +0200

    add typage for form addWilder

--- src/components/AddWilder.tsx
+++ src/components/AddWilder.tsx
@@ -14,7 +14,7 @@ function AddWilder(): JSX.Element {
   const [loading, setLoading] = useState(false);
   return (
     <Form
-      onSubmit={async (e) => {
+      onSubmit={async (e: React.SyntheticEvent) => {
         e.preventDefault();
         try {
           setLoading(true);
@@ -47,7 +47,9 @@ function AddWilder(): JSX.Element {
         type="text"
         placeholder="Type the name"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setName(e.target.value)
+        }
       />
       <Label htmlFor="city-input">City :</Label>
       <Input
@@ -55,7 +57,9 @@ function AddWilder(): JSX.Element {
         type="text"
         placeholder="Type the city"
         value={city}
-        onChange={(e) => setCity(e.target.value)}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setCity(e.target.value)
+        }
       />
       {error !== "" && <Error>{error}</Error>}
       <Button loading={loading}>
