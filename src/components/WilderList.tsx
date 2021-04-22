b7c5d7c368a43e9fb34953ae30e19f230f8c7a38
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2021-04-22 22:16:15 +0200

    getAllWilders - GraphQl

--- src/components/WilderList.tsx
+++ src/components/WilderList.tsx
@@ -1,36 +1,47 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
 import Wilder, { WilderProps } from "./Wilder";
-import { CardRow, Container } from "../styles/elements";
+import { CardRow, Container, Footer } from "../styles/elements";
+
+const ALL_WILDERS = gql`
+  query GetAllWilders {
+    allWilders {
+      id
+      name
+      skills {
+        title
+        votes
+      }
+    }
+  }
+`;
 
 function WilderList(): JSX.Element {
-  const [wilders, setWilders] = useState<WilderProps[]>([]);
-  useEffect(() => {
-    const fetchWilders = async () => {
-      try {
-        const result = await axios("http://localhost:5000/api/wilders");
-        setWilders(result.data.result);
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(error);
-      }
-    };
-    fetchWilders();
-  }, []);
+  const { loading, error, data } = useQuery(ALL_WILDERS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
   return (
-    <Container>
-      <CardRow>
-        {wilders.map((wilder) => (
-          <Wilder
-            key={wilder._id}
-            name={wilder.name}
-            city={wilder.city}
-            skills={wilder.skills}
-          />
-        ))}
-      </CardRow>
-    </Container>
+    <div>
+      <Container>
+        <h2>Wilders</h2>
+        <CardRow>
+          {data.allWilders.map((wilder: WilderProps) => (
+            <Wilder
+              key={wilder._id}
+              name={wilder.name}
+              city={wilder.city}
+              skills={wilder.skills}
+            />
+          ))}
+        </CardRow>
+      </Container>
+      <Footer>
+        <Container>
+          <p>&copy; 2020 Wild Code School</p>
+        </Container>
+      </Footer>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: "http://localhost:9002/graphql",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
